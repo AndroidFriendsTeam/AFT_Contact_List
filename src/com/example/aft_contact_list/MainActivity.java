1507fa28892961cc43418c3841ed11ac57395b4e
jeanlouispy <jeanlouispy@hotmail.com>
2013-02-20 14:24:27 +0100

    Add permission READ_CONTACTS in the manifest Implementation of contact selection process

--- src/com/example/aft_contact_list/MainActivity.java
+++ src/com/example/aft_contact_list/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.aft_contact_list;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+
+	private static final int PICK_CONTACT = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.btn_select_contact).setOnClickListener(this);
+		
 	}
 
 	@Override
@@ -18,5 +29,49 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Stub de la méthode généré automatiquement
+		switch (arg0.getId())
+		{
+		case R.id.btn_select_contact : selectContact();
+			break;
+		default :
+		}
+	}
+	
+	public void selectContact()
+	{
+		Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+		startActivityForResult(intent,PICK_CONTACT);
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		switch(requestCode)
+		{
+		case PICK_CONTACT :
+			if(resultCode == Activity.RESULT_OK)
+			{				
+				  Uri contactData = data.getData();
+			        Cursor c =  managedQuery(contactData, null, null, null, null);
+			        if (c.moveToFirst()) {
+			          String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+			          Toast.makeText(this,name,Toast.LENGTH_SHORT).show();
+			        }
+			}
+			break;
+		
+			
+		}
+		
+	}
+	
+	
 
 }
